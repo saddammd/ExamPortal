95200668ebe6a95511bae357bcd3fda339f91e22
saddammdjp <saddam.md.jp@gmail.com>
2022-12-27 08:14:08 +0530

    created modal class

---
+++ src/main/java/com/japanese/portal/Japanese/language/portal/JapaneseLanguagePortalApplication.java
@@ -0,0 +1,13 @@
+package com.japanese.portal.Japanese.language.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapaneseLanguagePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapaneseLanguagePortalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/japanese/portal/Japanese/language/portal/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.japanese.portal.Japanese.language.portal;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JapaneseLanguagePortalApplication.class);
+	}
+
+}
